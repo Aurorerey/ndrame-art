b694d34e34c72a591c82760357296a8dafb6cbdc
Aurore <aurore@lica-europe.org>
2022-03-15 18:11:01 -0500

    all pages and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+  def projet_1
+  end
+  def projet_2
+  end
+  def projet_3
+  end
+  def projet_4
+  end
+  def projet_5
+  end
+  def projet_6
+  end
+  def projet_7
+  end
+  def projet_8
+  end
+  def bio
+  end
+  def press
+  end
 end
