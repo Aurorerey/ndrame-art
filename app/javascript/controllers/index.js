d07dd64680208df4ede815ad4d56add8086c4770
Aurore <aurore@lica-europe.org>
2022-04-17 18:28:12 -0500

    stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,6 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-import 'bs5-lightbox';
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
